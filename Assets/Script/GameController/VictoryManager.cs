3b24e26e9258840bc7d401c69f4a27bb3b5cef10
Jeffez <contact@jetruccharasch.fr>
2024-12-30 02:26:16 +0100

    Debug Game Manager and Jump

--- Assets/Script/GameController/VictoryManager.cs
+++ Assets/Script/GameController/VictoryManager.cs
@@ -4,19 +4,22 @@ using UnityEngine.SceneManagement;
 
 public class VictoryManager : MonoBehaviour
 {
+    [SerializeField]
+    public int sceneLoad;
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.gameObject.CompareTag("Player"))
         {
             Debug.Log("Win");
+            DontDestroyOnLoad(GameManager.Instance);
             StartCoroutine(ChangeScene());
         }
     }
 
-
     IEnumerator ChangeScene()
     {
         yield return new WaitForSeconds(2);
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(sceneLoad);
     }
 }
